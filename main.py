f3e01b7054af2a48f01e07578e596efa3f4d8b78
artonge <artonge.C@gmail.com>
2016-12-09 17:49:39 +0100

    Add reconstruction from FFT

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from scipy.fftpack import fft2
+from scipy.fftpack import ifft2
 from scipy.fftpack import fft
 from scipy.fftpack import ifft
 from scipy.io import wavfile
@@ -22,12 +24,16 @@ def closestPowerOf2(n):
 fe, x = wavfile.read('photographe.wav') # Signal de la parole
 e = wavfile.read('chasse.wav') # Signal du son
 
-Tvoulu = 0.05  #  50ms              | 20ms(Duree d'un phoneme) < Tvoulu < 125ms(Frequence la plus basse de la voix d'un homme)
-Nvoulu = Tvoulu*fe   #  7200        | Nombre d'echantillon dans Tvoulu
-N  = closestPowerOf2(Nvoulu) # 2048 | Nombre d'ecahntillons dans une fenetre. On en garde seulement une puissance de 2 pour la fft
-T  = float(N) / float(fe) #  85.3ms | Duree d'une fenetre. On decoupe le signal en bouts de duree T = 85.3ms
-dt = T / 2.0  #  42.65ms            | Pas d'avancement dans le signal lors de la decoupe
-dn = N / 2    #  1024               | Pas d'avancement en echantillons
+
+Tvoulu = 0.05                                #  50ms     |  FLOAT  |  Duree theorique d'une fenetre
+Nvoulu = Tvoulu*fe                           #  2400     |  INT    |  Nombre d'echantillon dans Tvoulu
+N  = closestPowerOf2(Nvoulu)                 #  2048     |  INT    |  On rabaisse a la puissance de 2 la plus proche pour la fft
+T  = float(N) / float(fe)                    #  42.66ms  |  FLOAT  |  Duree d'une fenetre. On decoupe le signal en bouts de duree T = 85.3ms
+dt = T / 2.0                                 #  21.33ms  |  FLOAT  |  Duree du d'avancement dans le signal lors de la decoupe
+dn = N / 2                                   #  1024     |  INT    |  Pas d'avancement en echantillons
+xLen   = len(x)                              #  76776    |  INT    |  Nombre d'echantillion dans le signal
+nbFenetre = xLen / dn                        #  74       |  INT    |  Nombre de fenetres
+windowOverflow = xLen - nbFenetre * dn + dn  #  2024     |  INT    |  Nombre d'echantillons restant a la fin
 
 
 print 'Tvoulu -> ', Tvoulu
@@ -37,6 +43,9 @@ print 'N  -> ', N
 print 'T  -> ', T
 print 'dt -> ', dt
 print 'dn -> ', dn
+print 'len -> ', xLen
+print 'nbFenetre -> ', nbFenetre
+print 'windowOverflow -> ', windowOverflow
 
 
 # Fonction de hann
@@ -53,87 +62,58 @@ def hann(q):
 # Fonction de decoupe
 # m INT -> le numero de la decoupe
 # q INT -> le numero d'echantillon dans la decoupe
-# Return [INT INT]
-def u(m, q): return x[m * dn + q] * hann(q)
+# Return np.array: [INT INT]
+def u(m, q):
+    return x[m * dn + q] * hann(q)
 
 
 # Fonction de reconstruction d'un echantillon a partir de la FFT
 # m INT -> le numero de la decoupe
 # q INT -> le numero d'echantillon dans la decoupe
-# Return [INT INT]
+# Return np.array: [INT INT]
 recon_u_cache = {}
 def recon_u(m, q):
-    if m not in recon_u_cache:
-        recon_u_cache[m] = ifft(FFT_X(m))
+    if m not in recon_u_cache: recon_u_cache[m] = ifft2(FFT_X(m))
     return recon_u_cache[m][q]
 
 
 # Fonction de reconstruction d'un echantillon a partir des decoupes
 # n INT -> le numero de l'echantillon
-# Return [INT INT]
+# Return np.array: [INT INT]
 def recon_x(n):
     m = n / dn
     q = n - m * dn
-    return recon_u(m, q) + recon_u(m+1, q-dn)
+    return  recon_u(m, q) + recon_u(m+1, q-dn)
+
+
+# TF du signal x
+# m INT -> le numero de la decoupe
+# Return np.array: [[FLOAT FLOAT], ...]
+FFT_X_cache = {}
+def FFT_X(m):
+    if m not in FFT_X_cache:
+        _u = np.ndarray(shape=(N,2))
+        for q in range(0, N): _u[q] = u(m, q-dn)
+        FFT_X_cache[m] = fft2(_u)
+    return FFT_X_cache[m]
 
 
 # TF du signal x
 # m INT -> le numero de la decoupe
 # k INT -> indice voulu fans la Transform
-# Return [INT INT]
-def X(m, k):
-    _u = []
-    for q in range(0, N): _u.append(u(m, q-(N/2)))
-    return fft(_u)[k]
-
-
-# TF du signal x
-# m INT -> le numero de la decoupe
-# Return [[INT INT]]
-def FFT_X(m):
-    _u = []
-    for q in range(0, N): _u.append(u(m, q-(N/2)))
-    return fft(_u)
-
-
-m = 20
-k = 1
-X(m, k);
+# Return np.array: [FLOAT FLOAT]
+def X(m, k): return FFT_X(m)[k]
 
 
 
-
-
-
-
-
-
-
-
-# TEST RECONSTRUCTION DE X DEPUIS LE DECOUPAGE -- OK
-_x = []
-for i in range(0, len(x)):
-    print i
-    _x.append(recon_x(i))
-_x = np.array(_x)
-signalLength = x.shape[0]
-t = np.arange(0, signalLength, 1)
+# TEST RECONSTRUCTION DE X DEPUIS LE DECOUPAGE PUIS LA FFT
+_x = np.ndarray(shape=(xLen,2))
+for n in range(0, xLen-windowOverflow): _x[n] = recon_x(n).real
+print np.average(_x-x)
+t = np.arange(0, x.shape[0], 1)
 plt.figure(1)
 plt.subplot(211)
 plt.plot(t, x)
 plt.subplot(212)
 plt.plot(t, _x)
 plt.show()
-#################################################
-#################################################
-# TEST RECONSTRUCTION DE X DEPUIS LA FFT -- OK
-# _X = 0
-# for i in range(0, len(x)/N):
-#     if i == 0: _X = FFT_X(i)
-#     else: _X = np.append(_X, FFT_X(i))
-# 
-# signalLength = _X.shape[0]
-# t = np.arange(0, signalLength, 1)
-# plt.figure(1)
-# plt.plot(t, _X)
-# plt.show()
